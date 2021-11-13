aba3e5f3ec991140298c8aa09fb059da13169692
Tanmay Shirsat <93374610+TanmayShirsat@users.noreply.github.com>
2021-11-13 12:04:25 +0530

    Add files via upload

---
+++ tshirtMFun.js
@@ -0,0 +1,325 @@
+/*
+    function newImg() {
+      document.getElementById("img").src =
+        "	https://aeo.imgix.net/img/app/product/6/621280-6362543.jpg?w=317&auto=format";
+    }
+    function oldImg() {
+      document.getElementById("img").src =
+        "	https://aeo.imgix.net/img/app/product/6/621280-6362545.jpg?w=317&auto=format";
+    }
+    function newImg1() {
+      document.getElementById("img1").src =
+        "https://aeo.imgix.net/img/app/product/6/615407-5871852.jpg?w=317&auto=format";
+    }
+    function oldImg1() {
+      document.getElementById("img1").src =
+        "	https://aeo.imgix.net/img/app/product/6/615407-5871851.jpg?w=317&auto=format";
+    }
+    function newImg2() {
+      document.getElementById("img2").src =
+        "		https://aeo.imgix.net/img/app/product/6/612981-5831859.jpg?w=317&auto=format";
+    }
+    function oldImg2() {
+      document.getElementById("img2").src =
+        "		https://aeo.imgix.net/img/app/product/6/612981-5831858.jpg?w=317&auto=format";
+    }
+    function newImg3() {
+      document.getElementById("img3").src =
+        "	https://aeo.imgix.net/img/app/product/6/611446-5796072.jpg?w=317&auto=format";
+    }
+    function oldImg3() {
+      document.getElementById("img3").src =
+        "	https://aeo.imgix.net/img/app/product/6/611446-5796071.jpg?w=317&auto=format";
+    }
+    function newImg4() {
+      document.getElementById("img4").src =
+        "		https://aeo.imgix.net/img/app/product/6/611445-5796061.jpg?w=317&auto=format";
+    }
+    function oldImg4() {
+      document.getElementById("img4").src =
+        "		https://aeo.imgix.net/img/app/product/6/611445-5796060.jpg?w=317&auto=format";
+    }
+    function newImg5() {
+      document.getElementById("img5").src =
+        "	https://aeo.imgix.net/img/app/product/6/610431-5776233.jpg?w=317&auto=format";
+    }
+    function oldImg5() {
+      document.getElementById("img5").src =
+        "	https://aeo.imgix.net/img/app/product/6/610431-5776232.jpg?w=317&auto=format";
+    }
+    function newImg6() {
+      document.getElementById("img6").src =
+        "	https://aeo.imgix.net/img/app/product/6/610430-6093710.jpg?w=317&auto=format";
+    }
+    function oldImg6() {
+      document.getElementById("img6").src =
+        "	https://aeo.imgix.net/img/app/product/6/610430-6093712.jpg?w=317&auto=format";
+    }
+    function newImg7() {
+      document.getElementById("img7").src =
+        "	https://aeo.imgix.net/img/app/product/6/610429-6091397.jpg?w=317&auto=format";
+    }
+    function oldImg7() {
+      document.getElementById("img7").src =
+        "	https://aeo.imgix.net/img/app/product/6/610429-6091399.jpg?w=317&auto=format";
+    }
+    function newImg8() {
+      document.getElementById("img8").src =
+        "	https://aeo.imgix.net/img/app/product/6/610428-5776200.jpg?w=317&auto=format";
+    }
+    function oldImg8() {
+      document.getElementById("img8").src =
+        "https://aeo.imgix.net/img/app/product/6/610428-5776199.jpg?w=317&auto=format";
+    }
+    function newImg9() {
+      document.getElementById("img9").src =
+        "	https://aeo.imgix.net/img/app/product/6/606046-5696541.jpg?w=317&auto=format";
+    }
+    function oldImg9() {
+      document.getElementById("img9").src =
+        "https://aeo.imgix.net/img/app/product/6/606046-5696540.jpg?w=317&auto=format";
+    }
+    function newImg10() {
+      document.getElementById("img10").src =
+        "	https://aeo.imgix.net/img/app/product/6/605978-5695793.jpg?w=317&auto=format";
+    }
+    function oldImg10() {
+      document.getElementById("img10").src =
+        "https://aeo.imgix.net/img/app/product/6/605978-5695792.jpg?w=317&auto=format";
+    }
+    function newImg11() {
+      document.getElementById("img11").src =
+        "https://aeo.imgix.net/img/app/product/6/605960-6268838.jpg?w=317&auto=format";
+    }
+    function oldImg11() {
+      document.getElementById("img11").src =
+        "https://aeo.imgix.net/img/app/product/6/605960-6268840.jpg?w=317&auto=format";
+    }
+    function newImg12() {
+      document.getElementById("img12").src =
+        "https://aeo.imgix.net/img/app/product/6/605954-5695534.jpg?w=317&auto=format";
+    }
+    function oldImg12() {
+      document.getElementById("img12").src =
+        "https://aeo.imgix.net/img/app/product/6/605954-5695533.jpg?w=317&auto=format";
+    }
+    function newImg13() {
+      document.getElementById("img13").src =
+        "https://aeo.imgix.net/img/app/product/6/605304-6326500.jpg?w=317&auto=format";
+    }
+    function oldImg13() {
+      document.getElementById("img13").src =
+        "https://aeo.imgix.net/img/app/product/6/605304-6326503.jpg?w=317&auto=format";
+    }
+    function newImg14() {
+      document.getElementById("img14").src =
+        "	https://aeo.imgix.net/img/app/product/6/605303-6326486.jpg?w=317&auto=format";
+    }
+    function oldImg14() {
+      document.getElementById("img14").src =
+        "	https://aeo.imgix.net/img/app/product/6/605303-6326488.jpg?w=317&auto=format";
+    }
+    function newImg15() {
+      document.getElementById("img15").src =
+        "	https://aeo.imgix.net/img/app/product/6/605302-6326478.jpg?w=317&auto=format";
+    }
+    function oldImg15() {
+      document.getElementById("img15").src =
+        "	https://aeo.imgix.net/img/app/product/6/605302-6326470.jpg?w=317&auto=format";
+    }
+    function newImg16() {
+      document.getElementById("img16").src =
+        "https://aeo.imgix.net/img/app/product/6/604931-6326524.jpg?w=317&auto=format";
+    }
+    function oldImg16() {
+      document.getElementById("img16").src =
+        "https://aeo.imgix.net/img/app/product/6/604931-6326526.jpg?w=317&auto=format";
+    }
+    function newImg17() {
+      document.getElementById("img17").src =
+        "	https://aeo.imgix.net/img/app/product/6/604930-6268810.jpg?w=317&auto=format";
+    }
+    function oldImg17() {
+      document.getElementById("img17").src =
+        "	https://aeo.imgix.net/img/app/product/6/604930-6268812.jpg?w=317&auto=format";
+    }
+    function newImg18() {
+      document.getElementById("img18").src =
+        "https://aeo.imgix.net/img/app/product/6/604929-6326534.jpg?w=317&auto=format";
+    }
+    function oldImg18() {
+      document.getElementById("img18").src =
+        "https://aeo.imgix.net/img/app/product/6/604929-6326536.jpg?w=317&auto=format";
+    }
+    function newImg19() {
+      document.getElementById("img19").src =
+        "https://aeo.imgix.net/img/app/product/6/604928-5683073.jpg?w=317&auto=format";
+    }
+    function oldImg19() {
+      document.getElementById("img19").src =
+        "https://aeo.imgix.net/img/app/product/6/604928-5683072.jpg?w=317&auto=format";
+    }
+    function newImg21() {
+      document.getElementById("img21").src =
+        "	https://aeo.imgix.net/img/app/product/6/604927-5683062.jpg?w=317&auto=format";
+    }
+    function oldImg21() {
+      document.getElementById("img21").src =
+        "	https://aeo.imgix.net/img/app/product/6/604927-5683061.jpg?w=317&auto=format";
+    }
+    function newImg20() {
+      document.getElementById("img20").src =
+        "	https://aeo.imgix.net/img/app/product/6/604926-6326515.jpg?w=317&auto=format";
+    }
+    function oldImg20() {
+      document.getElementById("img20").src =
+        "https://aeo.imgix.net/img/app/product/6/604926-6326517.jpg?w=317&auto=format";
+    }*/
+   
+   
+   
+   
+    var data = [
+      {
+        name: "AE Embroidered T-Shirt",
+        rating: 4,
+        image1_url:"https://aeo.imgix.net/img/app/product/6/621280-6362545.jpg?w=317&auto=format",
+        image2_url:"https://aeo.imgix.net/img/app/product/6/621280-6362543.jpg?w=317&auto=format",
+        price: 1299,
+      },
+      {
+          name: "AE Embroidered T-Shirt",
+          rating: 3,
+          image1_url:"https://aeo.imgix.net/img/app/product/6/615407-5871851.jpg?w=317&auto=format",
+          image2_url:"https://aeo.imgix.net/img/app/product/6/615407-5871852.jpg?w=317&auto=format",
+          price: 1299,
+      },
+      {
+          name: "AE Embroidered T-Shirt",
+          rating: 4,
+          image1_url:"https://aeo.imgix.net/img/app/product/6/612981-5831858.jpg?w=317&auto=format",
+          image2_url:"https://aeo.imgix.net/img/app/product/6/612981-5831859.jpg?w=317&auto=format",
+          price: 1299,
+      },
+      {
+          name: "AE Super Soft Pocket T-Shirt",
+          rating: 4,
+          image1_url:"https://aeo.imgix.net/img/app/product/6/611445-5796071.jpg?w=317&auto=format",
+          image2_url:"https://aeo.imgix.net/img/app/product/6/611445-5796072.jpg?w=317&auto=format",
+          price: 11499,
+      }, 
+      {
+          name: "AE Super Soft Pocket T-Shirt",
+          rating: 4,
+          image1_url:"https://aeo.imgix.net/img/app/product/6/621280-6362560.jpg?w=317&auto=format",
+          image2_url:"https://aeo.imgix.net/img/app/product/6/621280-6362561.jpg?w=317&auto=format",
+          price: 11499,
+      },
+      {
+          name: "AE Super Soft Pocket T-Shirt",
+          rating: 4,
+          image1_url:"https://aeo.imgix.net/img/app/product/6/610431-5776232.jpg?w=317&auto=format",
+          image2_url:"https://aeo.imgix.net/img/app/product/6/610431-5776233.jpg?w=317&auto=format",
+          price: 11499,
+      },
+    ];
+  localStorage.setItem("shoppingBag" , JSON.stringify(data))
+  
+  var prod = JSON.parse(localStorage.getItem("shoppingBag")) || [];
+  /*
+  function additem(){
+      console.log("just check");
+      console.log(data);
+      for(i = 0; i<data.length; i++){
+          prod.push(data[i]);
+      }
+      localStorage.setItem("shoppingBag",JSON.stringify(prod))
+  }*/
+  //console.log(additem());
+  
+  displayItems(prod);
+  function displayItems(product) {
+      var maincon = document.querySelector("#dis");
+      maincon.innerHTML = "";  
+      product.map(function (item) {
+        var div = document.createElement("div")
+        var h1 = document.createElement("h1");
+        h1.textContent = item.name;
+        var h2 = document.createElement("h2");
+        h2.textContent = item.price;
+        var img = document.createElement("img");
+        img.setAttribute("src", item.image_url);
+        div.append(h1, h2, img);
+        maincon.append(div);
+      });
+  
+  }
+  
+  
+  
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+  
+  
+  function pricesort(){
+
+      var selected = document.getElementById("sel").value;
+      console.log(selected);
+      product.map(function (item) {
+      if(selected == "low"){
+        item.price = item.price.sort(function(a,b){
+            return a.price - b.price;
+        });
+        displayItems(prod);
+      }
+      if(selected == "high"){
+        item.price = item.price.sort(function(a,b){
+            return b.price - a.price;
+        });
+        displayItems(prod);
+      }
+    })
+      
+  }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
